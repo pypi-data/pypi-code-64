578ab16add1d1d44743ce56f341b980b96b2430d
Tom Forbes <tom@tomforb.es>
2020-09-20 13:13:52 +0000

    {"name":"airflowdoczh","version":"2020.9.20.0","file":"AirflowDocZh-2020.9.20.0-py3-none-any.whl","path":"airflowdoczh/AirflowDocZh-2020.9.20.0-py3-none-any.whl"}

---
+++ airflowdoczh/AirflowDocZh-2020.9.20.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+
+"""AirflowDocZh
+https://github.com/apachecn/airflow-doc-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2020.9.20.0"
