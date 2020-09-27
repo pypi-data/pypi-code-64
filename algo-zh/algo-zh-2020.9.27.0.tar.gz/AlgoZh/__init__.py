3d2b2213814a0486f0e7a89ad3d55306ec7c300b
Tom Forbes <tom@tomforb.es>
2020-09-27 13:49:03 +0000

    {"name":"algo-zh","version":"2020.9.27.0","file":"algo-zh-2020.9.27.0.tar.gz","path":"algo-zh/algo-zh-2020.9.27.0.tar.gz"}

---
+++ algo-zh/algo-zh-2020.9.27.0.tar.gz/AlgoZh/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""algo-zh
+https://github.com/apachecn/algo-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2020.9.27.0"
